f67b0143661931b574d42bf80fe486a999777b4a
土屋光暉 <nadegata@tsuchiyahikariakiranomacbook-air.local>
2018-07-13 16:21:31 +0900

    Initial Commit

---
+++ UTESTests/UTESTests.swift
@@ -0,0 +1,36 @@
+//
+//  UTESTests.swift
+//  UTESTests
+//
+//  Created by 土屋光暉 on 2018/07/13.
+//  Copyright © 2018年 mitsuki.com. All rights reserved.
+//
+
+import XCTest
+@testable import UTES
+
+class UTESTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
